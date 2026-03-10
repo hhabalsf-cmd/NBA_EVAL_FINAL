131a4bd3db3b3826a6796c35b43152bc9d8c58c2
hhabal-cmd <hhabalsf@gmail.com>
2026-03-10 01:59:45 -0700

    feat: add /api/sync/nightly endpoint for Supabase pg_cron

--- api/routers/__init__.py
+++ api/routers/__init__.py
@@ -5,5 +5,6 @@ from .picks import router as picks_router
 from .games import router as games_router
 from .auth import router as auth_router
 from .parlays import router as parlays_router
+from .sync import router as sync_router
 
-__all__ = ['players_router', 'bets_router', 'picks_router', 'games_router', 'auth_router', 'parlays_router']
+__all__ = ['players_router', 'bets_router', 'picks_router', 'games_router', 'auth_router', 'parlays_router', 'sync_router']
